5bf57e54cad2f8a070922c3fb9be7e1a066c5c25
DanPaul12 <dan.paul.schechter@gmail.com>
2024-06-30 23:52:08 -0700

    hw4

---
+++ classlist.py
@@ -0,0 +1,7 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and attended: 
+    print("Alice submitted and attended")
+else: 
+    print("Alice either failed to submit, attend, or both")
---
+++ numberlist.py
@@ -0,0 +1,7 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print(grades)
+
+grades_avg = sum(grades) / 10
+print(grades_avg)
